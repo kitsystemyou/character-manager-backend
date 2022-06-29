cf48b53f953991c8c7ce5e3c72f72a3db22a1144
kitsystemyou <kitsystemyou@gmail.com>
2022-06-30 02:10:42 +0900

    convert name blog to character

--- app/flaskr/character.py
+++ app/flaskr/character.py
@@ -3,21 +3,20 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from . import model
+from . import model, db
 from sqlalchemy import desc
 from datetime import datetime
 from flaskr.auth import login_required
 from flaskr.db import get_db
+from flaskr.model import Characters
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('character', __name__)
 
 
 @bp.route('/')
 def index():
-    db = get_db()
-    characters = db.query(model.Characters).order_by(desc(model.Characters.id))
-    print(characters)
-    return render_template('blog/index.html', characters=characters)
+    characters = Characters.query.order_by(desc(model.Characters.id))
+    return render_template('character/index.html', characters=characters)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -35,43 +34,40 @@ def create():
         if error is not None:
             flash(error)
         else:
-            db = get_db()
-            character = model.Characters()
-            character.id = None
-            character.user_id = session['user_id']
-            character.character_name = character_name
-            character.player_name = player_name
-            character.game_system = "CoC"
-            character.prof_img_path = ""
-            character.tags = tags
-            character.create_time = datetime.now()
-            character.update_time = datetime.now()
-            character.delete_time = None
-            db.add(character)
-            db.commit()
-            return redirect(url_for('blog.index'))
+            character = Characters(
+                id=None,
+                user_id=session['user_id'],
+                character_name=character_name,
+                player_name=player_name,
+                game_system="CoC",
+                prof_img_path="",
+                tags=tags,
+                create_time=datetime.now(),
+                update_time=datetime.now(),
+                delete_time=None,
+            )
+            db.session.add(character)
+            db.session.commit()
+            return redirect(url_for('character.index'))
 
-    return render_template('blog/create.html')
+    return render_template('character/create.html')
 
 
 def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        f' WHERE p.id = {id}'
-    ).fetchone()
+    post = Characters.query.filter_by(id=id).first()
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    print(post)
+    if check_author and post.user_id != str(g.user.id):
         abort(403)
 
     return post
 
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
+@ bp.route('/<string:id>/update', methods=('GET', 'POST'))
+@ login_required
 def update(id):
     post = get_post(id)
 
@@ -91,9 +87,9 @@ def update(id):
                 f"UPDATE post SET title = '{title}', body = '{body}' WHERE id = {id}"
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('character.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('character/update.html', post=post)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
@@ -103,4 +99,4 @@ def delete(id):
     db = get_db()
     db.execute(f'DELETE FROM post WHERE id = {id}')
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for('character.index'))
