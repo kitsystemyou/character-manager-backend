bc6b2d7b27f9829bc36c60b89664475632b6eed1
kitsystemyou <kitsystemyou@gmail.com>
2022-08-04 02:42:52 +0900

    Impl create character info

--- app/flaskr/character.py
+++ app/flaskr/character.py
@@ -4,12 +4,12 @@ from flask import (
 from werkzeug.exceptions import abort
 from http import HTTPStatus
 from . import model, db
-from sqlalchemy import create_engine, desc
+from sqlalchemy import desc
 from datetime import datetime
 from flaskr.auth import login_required
 from flaskr.model import (
     CharacterSchema, Characters, CocMetaInfo, CocMetaInfoSchema,
-    CocStatusParameters, CocStatusParametersSchema, CocSkills, CocSkillsSchema
+    CocStatusParameters, CocStatusParametersSchema, CocSkills
 )
 
 bp = Blueprint('character', __name__)
@@ -21,108 +21,7 @@ def index():
     return render_template('character/index.html', characters=characters)
 
 
-@bp.route('/test', methods=['POST'])
-def create_character_info():
-    req_json = request.get_json()
-
-    # TODO: Transaction
-    # Create Character
-    if not req_json["character"]:
-        error = "character is required."
-        flash(error)
-    else:
-        character_data = req_json["character"]
-    character = Characters(
-        user_id=character_data["user_id"],
-        character_name=character_data["character_name"],
-        player_name=character_data["player_name"],
-        game_system=character_data["game_system"],
-        prof_img_path=character_data["prof_img_path"],
-        tags=character_data["tags"],
-        create_time=datetime.now(),
-        update_time=datetime.now(),
-    )
-    print(vars(character))
-    character_id = create_character(character)
-    print(character_id)
-
-    # Create coc_meta_info
-    if not req_json["character"]["coc_meta_info"]:
-        error = "character meta info is required."
-    else:
-        coc_meta_info_data = req_json["character"]["coc_meta_info"]
-    coc_meta_info = CocMetaInfo(
-        character_id=character_id,
-        job=coc_meta_info_data["job"],
-        sex=coc_meta_info_data["sex"],
-        age=coc_meta_info_data["age"],
-        height=coc_meta_info_data["height"],
-        weight=coc_meta_info_data["weight"],
-        hair_color=coc_meta_info_data["hair_color"],
-        eye_color=coc_meta_info_data["eye_color"],
-        skin_color=coc_meta_info_data["skin_color"],
-        home_place=coc_meta_info_data["home_place"],
-        mental_disorder=coc_meta_info_data["mental_disorder"],
-        edu_background=coc_meta_info_data["edu_background"],
-        memo=coc_meta_info_data["memo"],
-    )
-    create_meta_info(coc_meta_info)
-
-    # Create coc_status_params
-    if not req_json["character"]["coc_status_parameters"]:
-        error = "character meta info is required."
-    else:
-        coc_status_parameters = req_json["character"]["coc_status_parameters"]
-    coc_status_parameters = CocStatusParameters(
-        character_id=character_id,
-        str=coc_status_parameters["str"],
-        con=coc_status_parameters["con"],
-        pow=coc_status_parameters["pow"],
-        dex=coc_status_parameters["dex"],
-        app=coc_status_parameters["app"],
-        size=coc_status_parameters["size"],
-        inte=coc_status_parameters["inte"],
-        edu=coc_status_parameters["edu"],
-        hp=coc_status_parameters["hp"],
-        mp=coc_status_parameters["mp"],
-        init_san=coc_status_parameters["init_san"],
-        current_san=coc_status_parameters["current_san"],
-        idea=coc_status_parameters["idea"],
-        knowledge=coc_status_parameters["knowledge"],
-        damage_bonus=coc_status_parameters["damage_bonus"],
-        luck=coc_status_parameters["luck"],
-        max_job_point=coc_status_parameters["max_job_point"],
-        max_concern_point=coc_status_parameters["max_concern_point"],
-    )
-    create_coc_status_parameters(coc_status_parameters)
-
-    return {"res": character_data}, HTTPStatus.OK
-
-
-def create_character(character):
-    print("create_character")
-    db.session.add(character)
-    db.session.commit()
-    return character.id
-
-
-def create_meta_info(meta_info):
-    db.session.add(meta_info)
-    db.session.commit()
-    return {"res": meta_info}, HTTPStatus.OK
-
-
-def create_coc_status_parameters(coc_status_parameters):
-    db.session.add(coc_status_parameters)
-    db.session.commit()
-    return {"res": coc_status_parameters}, HTTPStatus.OK
-
-
-def create_skill(coc_skill):
-    return {"res": coc_skill}, HTTPStatus.OK
-
-
-@bp.route('/characters')
+@bp.route('/character')
 def list_character():
     characters = Characters.query.order_by(desc(model.Characters.id))
     return jsonify({'result': CharacterSchema(many=True).dump(characters)}), 200
@@ -168,36 +67,108 @@ def get_characters_info_status(ch_id):
 
 
 @bp.route('/character', methods=['POST'])
-@login_required
-def create():
-    character_name = request.form['character_name']
-    player_name = request.form['player_name']
-    tags = request.form['tags']
+def create_character_info():
+    # @login_required
     error = None
-    print(session['user_id'])
-    if not character_name:
-        error = 'character name is required.'
+    req_json = request.get_json()
+
+    # Create Character
+    if not req_json["character"]:
+        error = "character is required."
+        flash(error)
+    else:
+        character_data = req_json["character"]
+    character = Characters(
+        user_id=character_data["user_id"],
+        character_name=character_data["character_name"],
+        player_name=character_data["player_name"],
+        game_system=character_data["game_system"],
+        prof_img_path=character_data["prof_img_path"],
+        tags=character_data["tags"],
+        create_time=datetime.now(),
+        update_time=datetime.now(),
+    )
+    print(vars(character))
+    # TODO: もしくは　character は作成済み前提でもいいかもしれない
+    create_character(character)
+    db.session.commit()
+    id = character.id
+    print(id)
+
+    # Create coc_meta_info
+    if not req_json["character"]["coc_meta_info"]:
+        error = "coc meta info is required."
+    else:
+        coc_meta_info_data = req_json["character"]["coc_meta_info"]
+    coc_meta_info = CocMetaInfo(
+        character_id=id,
+        job=coc_meta_info_data["job"],
+        sex=coc_meta_info_data["sex"],
+        age=coc_meta_info_data["age"],
+        height=coc_meta_info_data["height"],
+        weight=coc_meta_info_data["weight"],
+        hair_color=coc_meta_info_data["hair_color"],
+        eye_color=coc_meta_info_data["eye_color"],
+        skin_color=coc_meta_info_data["skin_color"],
+        home_place=coc_meta_info_data["home_place"],
+        mental_disorder=coc_meta_info_data["mental_disorder"],
+        edu_background=coc_meta_info_data["edu_background"],
+        memo=coc_meta_info_data["memo"],
+    )
+    create_meta_info(coc_meta_info)
+
+    # Create coc_status_params
+    if not req_json["character"]["coc_status_parameters"]:
+        error = "coc status parameters are required."
+    else:
+        coc_status_parameters = req_json["character"]["coc_status_parameters"]
+    coc_status_parameters = CocStatusParameters(
+        character_id=id,
+        str=coc_status_parameters["str"],
+        con=coc_status_parameters["con"],
+        pow=coc_status_parameters["pow"],
+        dex=coc_status_parameters["dex"],
+        app=coc_status_parameters["app"],
+        size=coc_status_parameters["size"],
+        inte=coc_status_parameters["inte"],
+        edu=coc_status_parameters["edu"],
+        hp=coc_status_parameters["hp"],
+        mp=coc_status_parameters["mp"],
+        init_san=coc_status_parameters["init_san"],
+        current_san=coc_status_parameters["current_san"],
+        idea=coc_status_parameters["idea"],
+        knowledge=coc_status_parameters["knowledge"],
+        damage_bonus=coc_status_parameters["damage_bonus"],
+        luck=coc_status_parameters["luck"],
+        max_job_point=coc_status_parameters["max_job_point"],
+        max_concern_point=coc_status_parameters["max_concern_point"],
+    )
+    create_coc_status_parameters(coc_status_parameters)
 
     if error is not None:
-        flash(error)
-        return jsonify({"result": "unknown error"}), HTTPStatus.INTERNAL_SERVER_ERROR
-    else:
-        character = Characters(
-            id=None,
-            user_id=session['user_id'],
-            character_name=character_name,
-            player_name=player_name,
-            game_system="CoC",
-            prof_img_path="",
-            tags=tags,
-            create_time=datetime.now(),
-            update_time=datetime.now(),
-            delete_time=None,
-        )
-        db.session.add(character)
-        db.session.commit()
+        return {"Failed to create character": error}, 500
+    db.session.commit()
+    return {"res": character_data}, HTTPStatus.OK
 
-        return redirect(url_for('character.index'))
+
+def create_character(character):
+    print("create_character")
+    db.session.add(character)
+    return
+
+
+def create_meta_info(meta_info):
+    db.session.add(meta_info)
+    return
+
+
+def create_coc_status_parameters(coc_status_parameters):
+    db.session.add(coc_status_parameters)
+    return
+
+
+def create_skill(coc_skill):
+    return
 
 
 def get_character(id, check_author=True):
