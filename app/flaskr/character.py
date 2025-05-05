b0fa20833add89f9d6786628e10972dc169096a9
singsignalyou <kitsystemyou@gmail.com>
2025-05-05 10:11:51 +0000

    fix create character api change validate

--- app/flaskr/character.py
+++ app/flaskr/character.py
@@ -94,105 +94,111 @@ def create_character_info():
     error = None
     req_json = request.get_json()
 
-    # Create Character
-    if not req_json["character"]:
+    # Check if character exists in the request
+    if not req_json.get("character"):
         error = "character is required."
         flash(error)
-    else:
-        character_data = req_json["character"]
+        return {"Failed to create character": error}, 500
+
+    # Get character data
+    character_data = req_json["character"]
+
+    # Check if character_name exists
+    if not character_data.get("character_name"):
+        error = "character_name is required."
+        flash(error)
+        return {"Failed to create character": error}, 500
+
+    # Create Character with only character_name as required
     character = Characters(
-        user_id=character_data["user_id"],
+        user_id=character_data.get("user_id"),
         character_name=character_data["character_name"],
-        player_name=character_data["player_name"],
-        game_system=character_data["game_system"],
-        prof_img_path=character_data["prof_img_path"],
-        tags=character_data["tags"],
+        player_name=character_data.get("player_name"),
+        game_system=character_data.get("game_system"),
+        prof_img_path=character_data.get("prof_img_path"),
+        tags=character_data.get("tags"),
         create_time=datetime.now(),
         update_time=datetime.now(),
     )
+
     print(vars(character))
-    # TODO: もしくは　character は作成済み前提でもいいかもしれない
     create_character(character)
     db.session.commit()
     id = character.id
     print("character_id", id)
 
-    # Create coc_meta_info
-    if not req_json["character"]["coc_meta_info"]:
-        error = "coc meta info is required."
-    else:
-        coc_meta_info_data = req_json["character"]["coc_meta_info"]
-    coc_meta_info = CocMetaInfo(
-        character_id=id,
-        job=coc_meta_info_data["job"],
-        sex=coc_meta_info_data["sex"],
-        age=coc_meta_info_data["age"],
-        height=coc_meta_info_data["height"],
-        weight=coc_meta_info_data["weight"],
-        hair_color=coc_meta_info_data["hair_color"],
-        eye_color=coc_meta_info_data["eye_color"],
-        skin_color=coc_meta_info_data["skin_color"],
-        home_place=coc_meta_info_data["home_place"],
-        mental_disorder=coc_meta_info_data["mental_disorder"],
-        edu_background=coc_meta_info_data["edu_background"],
-        memo=coc_meta_info_data["memo"],
-    )
-    create_meta_info(coc_meta_info)
-
-    # Create coc_status_params
-    if not req_json["character"]["coc_status_parameters"]:
-        error = "coc status parameters are required."
-    else:
-        coc_status_parameters = req_json["character"]["coc_status_parameters"]
-    # TODO: VALIDATION
-    if coc_status_parameters["max_job_point"] == "":
-        coc_status_parameters["max_job_point"] = 0
-    if coc_status_parameters["max_concern_point"] == "":
-        coc_status_parameters["max_concern_point"] = 0
-
-    coc_status_parameters = CocStatusParameters(
-        character_id=id,
-        str=coc_status_parameters["str"],
-        con=coc_status_parameters["con"],
-        pow=coc_status_parameters["pow"],
-        dex=coc_status_parameters["dex"],
-        app=coc_status_parameters["app"],
-        size=coc_status_parameters["size"],
-        inte=coc_status_parameters["int"],  # CAUTION
-        edu=coc_status_parameters["edu"],
-        hp=coc_status_parameters["hp"],
-        mp=coc_status_parameters["mp"],
-        init_san=coc_status_parameters["init_san"],
-        current_san=coc_status_parameters["current_san"],
-        idea=coc_status_parameters["idea"],
-        knowledge=coc_status_parameters["knowledge"],
-        damage_bonus=coc_status_parameters["damage_bonus"],
-        luck=coc_status_parameters["luck"],
-        max_job_point=coc_status_parameters["max_job_point"],
-        max_concern_point=coc_status_parameters["max_concern_point"],
-    )
-    create_coc_status_parameters(coc_status_parameters)
-
-    if not req_json["character"]["coc_skills"]:
-        error = "coc skills are required."
-    else:
-        coc_skills = req_json["character"]["coc_skills"]
-    for skill_data in coc_skills:
-        skill = CocSkills(
+    # Create coc_meta_info if exists
+    if character_data.get("coc_meta_info"):
+        coc_meta_info_data = character_data["coc_meta_info"]
+        coc_meta_info = CocMetaInfo(
             character_id=id,
-            skill_name=skill_data["skill_name"],
-            job_point=skill_data["job_point"],
-            concern_point=skill_data["concern_point"],
-            grow=skill_data["grow"],
-            other=skill_data["other"],
-            skill_type=skill_data.get("skill_type", 0),
+            job=coc_meta_info_data.get("job"),
+            sex=coc_meta_info_data.get("sex"),
+            age=coc_meta_info_data.get("age"),
+            height=coc_meta_info_data.get("height"),
+            weight=coc_meta_info_data.get("weight"),
+            hair_color=coc_meta_info_data.get("hair_color"),
+            eye_color=coc_meta_info_data.get("eye_color"),
+            skin_color=coc_meta_info_data.get("skin_color"),
+            home_place=coc_meta_info_data.get("home_place"),
+            mental_disorder=coc_meta_info_data.get("mental_disorder"),
+            edu_background=coc_meta_info_data.get("edu_background"),
+            memo=coc_meta_info_data.get("memo"),
         )
-        create_skill(skill)
+        create_meta_info(coc_meta_info)
 
-    if error is not None:
-        return {"Failed to create character": error}, 500
+    # Create coc_status_params if exists
+    if character_data.get("coc_status_parameters"):
+        coc_status_parameters = character_data["coc_status_parameters"]
+        
+        # Set default values for empty fields
+        max_job_point = coc_status_parameters.get("max_job_point", 0)
+        if max_job_point == "":
+            max_job_point = 0
+            
+        max_concern_point = coc_status_parameters.get("max_concern_point", 0)
+        if max_concern_point == "":
+            max_concern_point = 0
+
+        coc_status_params = CocStatusParameters(
+            character_id=id,
+            str=coc_status_parameters.get("str"),
+            con=coc_status_parameters.get("con"),
+            pow=coc_status_parameters.get("pow"),
+            dex=coc_status_parameters.get("dex"),
+            app=coc_status_parameters.get("app"),
+            size=coc_status_parameters.get("size"),
+            inte=coc_status_parameters.get("int"),  # CAUTION
+            edu=coc_status_parameters.get("edu"),
+            hp=coc_status_parameters.get("hp"),
+            mp=coc_status_parameters.get("mp"),
+            init_san=coc_status_parameters.get("init_san"),
+            current_san=coc_status_parameters.get("current_san"),
+            idea=coc_status_parameters.get("idea"),
+            knowledge=coc_status_parameters.get("knowledge"),
+            damage_bonus=coc_status_parameters.get("damage_bonus"),
+            luck=coc_status_parameters.get("luck"),
+            max_job_point=max_job_point,
+            max_concern_point=max_concern_point,
+        )
+        create_coc_status_parameters(coc_status_params)
+
+    # Create coc_skills if exists
+    if character_data.get("coc_skills"):
+        coc_skills = character_data["coc_skills"]
+        for skill_data in coc_skills:
+            skill = CocSkills(
+                character_id=id,
+                skill_name=skill_data.get("skill_name"),
+                job_point=skill_data.get("job_point"),
+                concern_point=skill_data.get("concern_point"),
+                grow=skill_data.get("grow"),
+                other=skill_data.get("other"),
+                skill_type=skill_data.get("skill_type", 0),
+            )
+            create_skill(skill)
+
     db.session.commit()
-    character_data = req_json["character"]
     character_data["id"] = id
     return {"result": character_data}, HTTPStatus.OK
 
